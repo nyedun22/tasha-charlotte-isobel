1f20b2251c084e961985bb14704b892cb3b86314
Natasha Edun <natashaedun@Natashas-MacBook-Pro-2.local>
2022-03-08 00:56:34 +0000

    changed list to set

--- lottery.py
+++ lottery.py
@@ -1,24 +1,33 @@
+# import the random module
 import random
 
 
 class Lottery:
 
     def __init__(self):
-        self._lottery_numbers = []
+        self._lottery_numbers = lotto_nums
 
-    def lotto():
-        lottery_numbers = []
-        for i in range(0, 6):
-            number = random.randint(1, 50)
-            # Check if this number has been added to the empty list
-            while number in lottery_numbers:
-                number = random.sample(1, 50)
-            # now that we've got the unique number, append it to the list:
-            lottery_numbers.append(number)
+    def generate_lottery_numbers():
+        # create empty set to store values in
+        lotto_nums = set([])
 
-        print(lottery_numbers)
+        # while loop to run 6 times
+        while len(lotto_nums) < 6:
+            # random.randint() to generate the random numbers
+            num = random.randint(1, 50)
+            # append the randomly generated numbers to the lotton_nums list
+            lotto_nums.add(num)
+        print(lotto_nums)
 
-if __name__ == "__lotto__":
+    generate_lottery_numbers()
+
+
+# use a set instead of a list
+# for loop with a nested while loop with 'continue'
+def main():
+    Lottery.generate_lottery_numbers()
+
+
+if __name__ == "__main__":
     main()
-
 
